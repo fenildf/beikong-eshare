f4e325cea448bb352309f2960f3c76519a4d7736
fushang318 <fushang318@gmail.com>
2013-07-30 18:00:19 +0800

    fix select_course_intent.rb

--- app/models/select_course_intent.rb
+++ app/models/select_course_intent.rb
@@ -20,54 +20,55 @@ class SelectCourseIntent < ActiveRecord::Base
     end
 
     def set_first_select_course_intent(course)
-      return if !_check_course?(course)
-      self.select_course_intent.update_attributes :first_course => course
+      return false if !_check_course?(course)
+      _select_course_intent.update_attributes :first_course => course
     end
 
     def set_second_select_course_intent(course)
-      return if !_check_course?(course)
-      self.select_course_intent.update_attributes :second_course => course
+      return false if !_check_course?(course)
+      _select_course_intent.update_attributes :second_course => course
     end
 
     def set_third_select_course_intent(course)
-      return if !_check_course?(course)
-      self.select_course_intent.update_attributes :third_course => course
+      return false if !_check_course?(course)
+      _select_course_intent.update_attributes :third_course => course
     end
 
     def set_select_course_intent_opinion(opinion)
-      self.select_course_intent.update_attributes :opinion => opinion
+      _select_course_intent.update_attributes :opinion => opinion
     end
 
     # ----------------
 
     def first_select_course_intent
-      self.select_course_intent.first_course
+      _select_course_intent.first_course
     end
 
     def second_select_course_intent
-      self.select_course_intent.second_course
+      _select_course_intent.second_course
     end
 
     def third_select_course_intent
-      self.select_course_intent.third_course
+      _select_course_intent.third_course
     end
 
     def select_course_intent_opinion
-      self.select_course_intent.opinion
+      _select_course_intent.opinion
     end
 
     # ----------------
 
-    def select_course_intent
-      SelectCourseIntent.where(:user_id => self.id).first || SelectCourseIntent.create(:user => self)
-    end
 
     private
+      def _select_course_intent
+        self.select_course_intent || self.create_select_course_intent
+      end
+
       def _check_course?(course)
         approved      = Course.approved.include?(course)
-        check_first   = course != self.select_course_intent.first_course
-        check_second  = course != self.select_course_intent.second_course
-        check_third   = course != self.select_course_intent.third_course
+        check_first   = course != _select_course_intent.first_course
+        check_second  = course != _select_course_intent.second_course
+        check_third   = course != _select_course_intent.third_course
         approved && check_first && check_second && check_third
       end
   end
