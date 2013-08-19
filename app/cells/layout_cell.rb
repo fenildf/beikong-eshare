d53be5378dec9aad29cd5ba55e6b27553dcf2b50
ben7th <ben7th@sina.com>
2013-08-19 09:45:00 +0800

    分开两个登录界面

--- app/cells/layout_cell.rb
+++ app/cells/layout_cell.rb
@@ -7,6 +7,7 @@ class LayoutCell < Cell::Rails
 
   def topnav(opts = {})
     @user = opts[:user]
+    @for = opts[:for]
     render
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,9 +30,36 @@ class SessionsController < Devise::SessionsController
       return super
     end
 
+    if params[:to] == 'xuanke'
+      redirect_path = '/'
+    else
+      redirect_path = '/courses'
+    end
+
+    flash[:sign_in_to] = params[:sign_in_to]
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
-    render :json => {:sign_in => 'ok', :location => after_sign_in_path_for(resource)} # 必须响应 json 否则会被 jquery 判为 error
+    # render :json => {:sign_in => 'ok', :location => after_sign_in_path_for(resource)} # 必须响应 json 否则会被 jquery 判为 error
+    render :json => {:sign_in => 'ok', :location => redirect_path} # 必须响应 json 否则会被 jquery 判为 error
+  end
+
+  def destroy
+    # redirect_path = after_sign_out_path_for(resource_name)
+    if params[:to] == 'xuanke'
+      redirect_path = '/xk/sign_in'
+    else
+      redirect_path = '/eshare/sign_in'
+    end
+
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
+
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
   end
 end
