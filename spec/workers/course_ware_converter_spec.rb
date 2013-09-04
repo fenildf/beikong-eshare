bef4ff8f4d01f46fd60565b75e2f01762b770d24
kaid <kaid@kaid.me>
2013-09-04 09:49:05 +0000

    added doc convert support

--- spec/workers/course_ware_converter_spec.rb
+++ spec/workers/course_ware_converter_spec.rb
@@ -2,13 +2,14 @@ require 'spec_helper'
 require 'sidekiq/testing/inline'
 
 describe CourseWareConverter do
-  let(:file) {FactoryGirl.create :file_entity}
+  let(:file) {FactoryGirl.create :file_entity, :doc}
+  let(:path) {file.attach.path}
   before do
     Docsplit.stub(:extract_images).and_return ['haha.format']
   end
 
   it 'converts course ware to corresponding formats in background' do
-    Docsplit.should_receive(:extract_images).with(an_instance_of(String), an_instance_of(Hash))
+    Docsplit.should_receive(:extract_images).with(path, an_instance_of(Hash))
     CourseWareConverter.perform_async(file.id)
   end
 end
