378df92bc2fba11f83781597460e928623fcee68
fushang318 <fushang318@gmail.com>
2013-09-05 10:53:38 +0800

    抽取 practice_record 相关代码到 module

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,4 +1,5 @@
 class Practice < ActiveRecord::Base
+  include PracticeRecord::PracticeMethods
 
   attr_accessible :title, :content, :chapter, :creator, 
                   :attaches_attributes, :requirements_attributes
@@ -6,98 +7,17 @@ class Practice < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User', :foreign_key => :creator_id
   belongs_to :chapter
 
-
   has_many :attaches, :class_name => 'PracticeAttach', :foreign_key => :practice_id
   has_many :requirements, :class_name => 'PracticeRequirement', :foreign_key => :practice_id
-  has_many :records, :class_name => 'PracticeRecord', :foreign_key => :practice_id
   has_many :uploads, :through => :records
 
   accepts_nested_attributes_for :attaches
   accepts_nested_attributes_for :requirements
 
-
   validates :title, :chapter, :creator, :presence => true
-
 
   scope :by_creator, lambda{|creator| where(:creator_id => creator.id) }
   scope :by_course, lambda{|course| joins(:chapter).where('chapters.course_id = ?', course.id) }
-
-
-  def submit_by_user(user)
-    self.records.create(
-      :practice => self,
-      :user => user,
-      :submitted_at => Time.now,
-      :status => PracticeRecord::Status::SUBMITTED
-    )
-  end
-
-
-  def check_by_user(user, score,comment)
-    practice_record = get_record_by_user(user)
-
-    practice_record.status = PracticeRecord::Status::CHECKED
-    practice_record.checked_at = Time.now
-    practice_record.score = score
-    practice_record.comment = comment
-    practice_record.save
-  end
-
-
-  def in_submitted_status_of_user?(user)
-    return false if _empty_records_by_user?(user)
-    get_record_by_user(user).status == PracticeRecord::Status::SUBMITTED
-  end
-
-  def in_checked_status_of_user?(user)
-    return false if _empty_records_by_user?(user)
-    get_record_by_user(user).status == PracticeRecord::Status::CHECKED
-  end
-
-  def in_submitted_offline_of_user?(user)
-    return false if _empty_records_by_user?(user)
-    get_record_by_user(user).status == PracticeRecord::Status::SUBMITTED_OFFLINE
-  end
-
-  def submitted_time_by_user(user)
-    get_record_by_user(user).submitted_at
-  end
-
-  def checked_time_by_user(user)
-    get_record_by_user(user).checked_at
-  end
-
-
-  def submitted_users
-    records.where(:status => PracticeRecord::Status::SUBMITTED).map do |r|
-      r.user
-    end
-  end
-
-  def checked_users
-    records.where(:status => PracticeRecord::Status::CHECKED).map do |r|
-      r.user
-    end
-  end
-
-  def submittd_offline_by_user(user, submit_desc)
-    practice_record = get_record_by_user(user)
-    practice_record.status = PracticeRecord::Status::SUBMITTED_OFFLINE
-    practice_record.submit_desc = submit_desc
-    practice_record.is_submitted_offline = true
-    practice_record.submitted_offline_at = Time.now
-    practice_record.save
-  end
-
-  def get_record_by_user(user)    
-    self.records.where(:user_id => user.id).first
-  end
-
-  private
-    def _empty_records_by_user?(user)
-      self.records.where(:user_id => user.id).count == 0
-    end
-
 
   module UserMethods
     def self.included(base)
--- app/models/practice_record.rb
+++ app/models/practice_record.rb
@@ -25,4 +25,85 @@ class PracticeRecord < ActiveRecord::Base
     end
   end
 
+  module PracticeMethods
+    def self.included(base)
+      base.has_many :records, :class_name => 'PracticeRecord', :foreign_key => :practice_id
+    end
+
+    # 学生提交作业
+    def submit_by_user(user)
+      self.records.create(
+        :practice => self,
+        :user => user,
+        :submitted_at => Time.now,
+        :status => PracticeRecord::Status::SUBMITTED
+      )
+    end
+
+    # 学生声明已经线下提交了作业
+    def submittd_offline_by_user(user, submit_desc)
+      practice_record = get_record_by_user(user)
+      practice_record.status = PracticeRecord::Status::SUBMITTED_OFFLINE
+      practice_record.submit_desc = submit_desc
+      practice_record.is_submitted_offline = true
+      practice_record.submitted_offline_at = Time.now
+      practice_record.save
+    end
+
+    # 老师验收作业
+    def check_by_user(user, score,comment)
+      practice_record = get_record_by_user(user)
+
+      practice_record.status = PracticeRecord::Status::CHECKED
+      practice_record.checked_at = Time.now
+      practice_record.score = score
+      practice_record.comment = comment
+      practice_record.save
+    end
+
+    def in_submitted_status_of_user?(user)
+      return false if _empty_records_by_user?(user)
+      get_record_by_user(user).status == PracticeRecord::Status::SUBMITTED
+    end
+
+    def in_checked_status_of_user?(user)
+      return false if _empty_records_by_user?(user)
+      get_record_by_user(user).status == PracticeRecord::Status::CHECKED
+    end
+
+    def in_submitted_offline_of_user?(user)
+      return false if _empty_records_by_user?(user)
+      get_record_by_user(user).status == PracticeRecord::Status::SUBMITTED_OFFLINE
+    end
+
+    def submitted_time_by_user(user)
+      get_record_by_user(user).submitted_at
+    end
+
+    def checked_time_by_user(user)
+      get_record_by_user(user).checked_at
+    end
+
+    def submitted_users
+      records.where(:status => PracticeRecord::Status::SUBMITTED).map do |r|
+        r.user
+      end
+    end
+
+    def checked_users
+      records.where(:status => PracticeRecord::Status::CHECKED).map do |r|
+        r.user
+      end
+    end
+
+    def get_record_by_user(user)    
+      self.records.where(:user_id => user.id).first
+    end
+
+    private
+      def _empty_records_by_user?(user)
+        self.records.where(:user_id => user.id).count == 0
+      end
+  end
+
 end
