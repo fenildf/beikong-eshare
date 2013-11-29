be1b48eb070c514bfa204692da71352881f61d6a
fushang318 <fushang318@gmail.com>
2013-11-29 10:44:18 +0800

    完成导入分组用户脚本

--- script/pack5.rb
+++ script/pack5.rb
@@ -12,7 +12,50 @@ end
 
 class Util
   def self.import_user_by_data(data)
-    # TODO
+    if data[:role] == :teacher
+      group_kind = GroupTreeNode::TEACHER
+    elsif data[:role] == :student
+      group_kind = GroupTreeNode::STUDENT
+    end
+    group_str = data.delete :group
+
+    user = User.create!(data)
+    if !group_str.blank?
+      group = self.build_group(group_str, group_kind)
+      group.add_user(user)
+    end
+  end
+
+  # group_str 举例 "15届|split|高一|split|2班"
+  # 表示三级分组 "15届" => "高一" => "2班"
+  def self.build_group(group_str, group_kind)
+    group_names = group_str.split("|split|")
+
+    first_group = group_names.shift
+    # 创建或者查找根分组
+    current_group = GroupTreeNode.roots.find_by_name(first_group)
+    if current_group.blank?
+      current_group = GroupTreeNode.create!(
+        :name => first_group,
+        :kind => group_kind
+      )
+    end
+
+    # 创建或者查找下级分组
+    group_names.each do |group_name|
+      tg = current_group.children.find_by_name(group_name)
+      if tg.blank?
+        tg = GroupTreeNode.create!(
+          :name => group_name,
+          :kind => group_kind
+        )
+        tg.move_to_child_of(current_group)
+      end
+
+      current_group = tg
+    end
+
+    return current_group
   end
 
   def self.parse_excel(path, role)
