e472bb01e2581efa4d44d27de6ea1474c5a8df48
fushang318 <fushang318@gmail.com>
2013-11-29 12:11:27 +0800

    add some api

--- app/models/group_tree_node_user.rb
+++ app/models/group_tree_node_user.rb
@@ -12,6 +12,21 @@ class GroupTreeNodeUser < ActiveRecord::Base
   module UserMethods
     def self.included(base)
       base.has_many :group_tree_node_users
+      base.has_many :joined_group_tree_nodes,
+        :through => :group_tree_node_users,
+        :source  => :group_tree_node
+      base.scope :without_group, base.joins(
+        %`
+          left join 
+            group_tree_node_users
+          on
+            group_tree_node_users.user_id = users.id
+        `
+      ).where(%`
+        group_tree_node_users.user_id is null
+      `
+      )
     end
+
   end
 end
